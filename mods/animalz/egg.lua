ccb35de1db4f8718be9802a4cadbd9b7de553db7
Tim7900 <100736284+Tim79000@users.noreply.github.com>
2024-08-31 21:30:14 -0800

    add egg to satisfy player taste to help players endure the harsh landscape and egg will come from cows in the future so you can get a foodsource thats easy to cook and eat potentionally using ksurvive mechanics revolving around fire and it can possibly be eaten raw and that would be bad thought but maybe i will ad it!

---
+++ mods/animalz/egg.lua
@@ -0,0 +1,25 @@
+minetest.register_node("animalz:raw_egg", {
+	description = ("Raw Egg (not the twitter accoutn)"),
+	tiles = {"animalz_raw_egg.png"},
+	inventory_image = "animalz_raw_egg.png",
+	wield_image = "animalz_raw_egg.png",
+	paramtype = "light",
+	buildable_to = true,
+	floodable = true,
+	drawtype = "nodebox",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, -0.25, 0.5},
+		},
+	},
+	groups = {crumbly = 3, egg = 1, delicious = 1, falling_node = 1, tasty = 1, tastes_good = 1, i_like_egg = 1},
+--	sounds = default.node_sound_snow_defaults(), -- add sounds later because im busy
+
+	on_construct = function(pos)
+		pos.y = pos.y - 1
+		if minetest.get_node(pos).name == "default:dirt_with_grass" then
+			minetest.set_node(pos, {name = "default:dirt_with_snow"})
+		end
+	end,
+})
--- mods/animalz/init.lua
+++ mods/animalz/init.lua
@@ -3,3 +3,4 @@ local modpath = minetest.get_modpath("animalz")
 
 -- Cow
 dofile(modpath.."/cow.lua")
+dofile(modpath.."/egg.lua")
